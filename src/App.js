91d78193cdcb6b2e676924c3590101f3d3400ea6
anwarcse7 <anwarcse7@gmail.com>
2022-07-08 14:37:17 -0500

    header and footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import './App.css';
+import "./App.css";
 import { Link } from "react-router-dom";
-import Header from './components/header';
-import Footer from './components/footer';
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <h1>Hello</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/dashboard">Dashboard</Link> |{" "}
-        <Link to="/login">Login</Link> |{" "}
-        <Link to="/registration">Registration</Link> |{" "}
-        <Link to="/profile">Profile</Link>
-      </nav>
-      <Button variant="primary">Button #1</Button>
+      <Header />
+      <Container></Container>
       <Footer />
     </div>
   );
--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => (
   <div className="header">
-   
+      <Navbar bg="primary" variant="dark">
+        <Container>
+        <Navbar.Brand href="#">Home</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/dashboard">Dashboard</Nav.Link> 
+          <Nav.Link href="/login">Login</Nav.Link> 
+          <Nav.Link href="/registration">Registration</Nav.Link>
+          <Nav.Link href="/profile">Profile</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
   </div>
 )
 export default Header;
