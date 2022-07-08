2c8b505cf38ba2ccdeb7b2ac88a8a77abfd15ef3
anwarcse7 <anwarcse7@gmail.com>
2022-07-08 16:57:35 -0500

    login modify

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from "./components/footer";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import Form from 'react-bootstrap/Form'
 import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from 'react';
+
 function App() {
   return (
     <div className="App">
--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,8 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
-const Login = () => {
-  // return <h1>Login</h1>;
 
+const Login = () => {
   return (
     <Container>
       <Row>
@@ -17,18 +16,18 @@ const Login = () => {
               <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Control type="email" name="uname" placeholder="Enter email" />
                   <Form.Text className="text-muted"></Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
+                  <Form.Control name="pass" type="password" placeholder="Password" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                   <Form.Check type="checkbox" label="Remember Me" />
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Button variant="primary" type="submit" className="float-right">
                   Submit
                 </Button>
               </Form>
